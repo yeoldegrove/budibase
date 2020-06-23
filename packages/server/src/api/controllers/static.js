cc806db3faaed9f71aa7db65902e807028c7d36f
Mark O'Donnell <markodonnell@marks-mbp.home>
2020-06-23 12:18:57 +0100

    pulls and streams index.html + files from s3 bucket

--- packages/server/src/api/controllers/static.js
+++ packages/server/src/api/controllers/static.js
@@ -1,5 +1,6 @@
 const send = require("koa-send")
 const { resolve, join } = require("path")
+const fetch = require("node-fetch");
 const {
   budibaseAppsDir,
   budibaseTempDir,
@@ -13,15 +14,22 @@ exports.serveBuilder = async function(ctx) {
 }
 
 exports.serveApp = async function(ctx) {
-  // default to homedir
-  const appPath = resolve(
-    budibaseAppsDir(),
-    ctx.params.appId,
-    "public",
-    ctx.isAuthenticated ? "main" : "unauthenticated"
-  )
 
-  await send(ctx, ctx.file, { root: ctx.devPath || appPath })
+  // default to homedir
+  // const appPath = resolve(
+  //   budibaseAppsDir(),
+  //   ctx.params.appId,
+  //   "public",
+  //   ctx.isAuthenticated ? "main" : "unauthenticated"
+  // )
+  
+  const appId = ctx.request.header.host.split('.')[0];
+  const mainOrAuth = ctx.isAuthenticated ? "main" : "unauthenticated";
+  const requestUrl = `https://s3-eu-west-1.amazonaws.com/apps.budibase.com/${appId}/public/${mainOrAuth}/${ctx.file}`
+  const response = await fetch(requestUrl)
+  // if fetch fails - return error page
+  const body = await response.text()
+  ctx.body = body 
 }
 
 exports.serveComponentLibrary = async function(ctx) {
