0d76c42ae3bd60d9cac1af2eb5c4b14fbb1cf545
Mark O'Donnell <markodonnell@marks-mbp.home>
2020-06-24 15:18:27 +0100

    load assets from s3 - working locally confirmed

--- packages/server/src/api/controllers/static.js
+++ packages/server/src/api/controllers/static.js
@@ -5,15 +5,64 @@ const {
   budibaseAppsDir,
   budibaseTempDir,
 } = require("../../utilities/budibaseDir")
-const env = require("../../environment")
+const setBuilderToken = require("../../utilities/builder/setBuilderToken")
+const { ANON_LEVEL_ID } = require("../../utilities/accessLevels")
+const jwt = require("jsonwebtoken")
 
 exports.serveBuilder = async function(ctx) {
   let builderPath = resolve(__dirname, "../../../builder")
-  ctx.cookies.set("builder:token", env.ADMIN_SECRET)
+  if (ctx.file === "index.html") {
+    setBuilderToken(ctx)
+  }
   await send(ctx, ctx.file, { root: ctx.devPath || builderPath })
 }
 
 exports.serveApp = async function(ctx) {
+  // default to homedir
+  // const appPath = resolve(
+  //   budibaseAppsDir(),
+  //   ctx.params.appId,
+  //   "public",
+  //   ctx.isAuthenticated ? "main" : "unauthenticated"
+  // )
+  // only set the appId cookie for /appId .. we COULD check for valid appIds
+  // but would like to avoid that DB hit
+  const appId = ctx.request.header.host.split('.')[0];
+
+  // const looksLikeAppId = /^[0-9a-f]{32}$/.test(ctx.params.appId)
+  const looksLikeAppId = /^[0-9a-f]{32}$/.test(appId)
+  if (looksLikeAppId && !ctx.isAuthenticated) {
+    const anonUser = {
+      userId: "ANON",
+      accessLevelId: ANON_LEVEL_ID,
+      appId: appId
+    }
+    const anonToken = jwt.sign(anonUser, ctx.config.jwtSecret)
+    ctx.cookies.set("budibase:token", anonToken, {
+      path: "/",
+      httpOnly: false,
+    })
+  }
+
+  const mainOrAuth = ctx.isAuthenticated ? "main" : "unauthenticated";
+  const file = ctx.file ? ctx.file : "index.html"
+  
+  const requestUrl = `https://s3-eu-west-1.amazonaws.com/apps.budibase.com/${appId}/public/${mainOrAuth}/${file}`
+  console.log('request url:' , requestUrl)
+  const response = await fetch(requestUrl)
+  const body = await response.text()
+  ctx.body = body 
+  // await send(ctx, ctx.file || "index.html", { root: ctx.devPath || appPath })
+}
+
+exports.serveAppAsset = async function(ctx) {
+  // default to homedir
+  // const appPath = resolve(
+  //   budibaseAppsDir(),
+  //   ctx.user.appId,
+  //   "public",
+  //   ctx.isAuthenticated ? "main" : "unauthenticated"
+  // )
 
   // default to homedir
   // const appPath = resolve(
@@ -22,8 +71,7 @@ exports.serveApp = async function(ctx) {
   //   "public",
   //   ctx.isAuthenticated ? "main" : "unauthenticated"
   // )
-  
-  const appId = ctx.request.header.host.split('.')[0];
+  const appId = ctx.user.appId
   const mainOrAuth = ctx.isAuthenticated ? "main" : "unauthenticated";
   const requestUrl = `https://s3-eu-west-1.amazonaws.com/apps.budibase.com/${appId}/public/${mainOrAuth}/${ctx.file}`
   const response = await fetch(requestUrl)
@@ -34,21 +82,27 @@ exports.serveApp = async function(ctx) {
 
 exports.serveComponentLibrary = async function(ctx) {
   // default to homedir
-  let componentLibraryPath = resolve(
-    budibaseAppsDir(),
-    ctx.params.appId,
-    "node_modules",
-    decodeURI(ctx.query.library),
-    "dist"
-  )
+  // let componentLibraryPath = resolve(
+  //   budibaseAppsDir(),
+  //   ctx.user.appId,
+  //   "node_modules",
+  //   decodeURI(ctx.query.library),
+  //   "dist"
+  // )
+  // if (ctx.isDev) {
+  //   componentLibraryPath = join(
+  //     budibaseTempDir(),
+  //     decodeURI(ctx.query.library),
+  //     "dist"
+  //   )
+  // }
 
-  if (ctx.isDev) {
-    componentLibraryPath = join(
-      budibaseTempDir(),
-      decodeURI(ctx.query.library),
-      "dist"
-    )
-  }
-
-  await send(ctx, "/index.js", { root: componentLibraryPath })
+  const appId = ctx.user.appId
+  const requestUrl = encodeURI(`https://s3-eu-west-1.amazonaws.com/apps.budibase.com/${appId}/node_modules/${ctx.query.library}/dist/index.js`)
+  console.log('request url components: ', requestUrl)
+  const response = await fetch(requestUrl)
+  const body = await response.text()
+  ctx.type = 'application/javascript'
+  ctx.body = body;
+  // await send(ctx, "/index.js", { root: componentLibraryPath })
 }
